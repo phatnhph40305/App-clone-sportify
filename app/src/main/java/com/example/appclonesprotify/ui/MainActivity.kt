07bf5e8a9af9158bba8bf41fee5ac5b796268b10
phatnhph <phatnhph40305@fpt.edu.vn>
2025-01-09 18:45:44 +0700

    update ui

--- app/src/main/java/com/example/appclonesprotify/ui/MainActivity.kt
+++ app/src/main/java/com/example/appclonesprotify/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.appclonesprotify.ui
 
+import android.app.PendingIntent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.appclonesprotify.R
 import com.example.appclonesprotify.utils.token.AccessToken
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
             AccessToken.getAccessToken()
             Log.d("SpotifyToken", "Access Token: ${AccessToken.getAccessToken()}")
         }
+
 
 
 
